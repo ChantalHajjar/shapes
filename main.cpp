2b5dd51971e014afc85da861ee21daf848dbc595
SAAD <Chantal.Hajjar@usj.edu.lb>
2023-11-17 10:04:26 +0200

    Classe Shape

--- main.cpp
+++ main.cpp
@@ -4,15 +4,10 @@ using namespace std;
 
 class Point{
   public:
-    Point(double x, double y): x(x), y(y) 
-    { 
-        cout << "Point(double, double) called" << endl;
-    }
-    Point(double a = 0): x(a), y(a) {
-        cout << "Point(double) called" << endl;
-    }
-    ~Point(){cout << "Point destroyed" << endl;}
+    Point(double x, double y): x(x), y(y) {}
     
+    Point(double a = 0):  x(a),y(a) {}
+        
     string toString() const {
         stringstream ss;
         ss << "(" << x << ", " << y << ")";
@@ -23,11 +18,36 @@ class Point{
     double x;
     double y;
 };
+
 ostream& operator<<(ostream& os, const Point& p) {
     os << p.toString();
     return os;
 }
+class Shape{
+
+    public:
+    Shape(double x = 0, double y = 0, double elevation =0):c(x,y), elevation(elevation) {}
+    
+    string toString() const{
+        stringstream ss;
+        ss << "Centre:" << c << " Elevation:" << elevation << endl;
+        return ss.str();
+    }
+    private:
+    Point c;
+    double elevation;
+};
+ostream& operator<<(ostream& os, const Shape& s) {
+    return os << s.toString();
+}
+
 int main(){
     Point p1(1.2, 3.4);
-    cout << p1 << endl;
-}
+    Point p2;
+    Shape s1(1,2,5);
+    cout << s1 << endl;
+    Shape s2;
+    cout << s2 << endl;
+    return 0;
+}   
+
