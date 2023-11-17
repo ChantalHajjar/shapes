586ac7e7c5698b94fea7ca628c05da5588fa929a
SAAD <Chantal.Hajjar@usj.edu.lb>
2023-11-17 10:28:36 +0200

    Rectangle

--- main.cpp
+++ main.cpp
@@ -26,9 +26,13 @@ ostream& operator<<(ostream& os, const Point& p) {
 class Shape{
 
     public:
-    Shape(double x = 0, double y = 0, double elevation =0):c(x,y), elevation(elevation) {}
+    Shape(double x = 0, double y = 0, double elevation =0):c(x,y), elevation(elevation) {cout << "Shape created" << endl;}
     
-    string toString() const{
+    virtual double surface() const{
+        return -1;
+    }
+    protected:
+    string toStringS() const{
         stringstream ss;
         ss << "Centre:" << c << " Elevation:" << elevation << endl;
         return ss.str();
@@ -37,17 +41,38 @@ class Shape{
     Point c;
     double elevation;
 };
-ostream& operator<<(ostream& os, const Shape& s) {
-    return os << s.toString();
-}
+// ostream& operator<<(ostream& os, const Shape& s) {
+//     return os << s.toStringS();
+// }
+class Rectangle: public Shape{
+public:
+Rectangle(double x = 0, double y = 0, double elevation = 0, double width = 0, double height = 0):
+Shape(x,y,elevation), width(width), height(height) { cout << "Rectangle created" << endl;}
 
+string toString() const{
+    stringstream ss;
+    ss << toStringS() << " Width:" << width << " Height:" << height << endl;
+    return ss.str();
+}
+double surface() const{
+    return width*height;
+}
+private:
+    double width;
+    double height;
+};
+ostream& operator<<(ostream& os, const Rectangle& r) {
+     return os << r.toString();
+ }
 int main(){
     Point p1(1.2, 3.4);
     Point p2;
-    Shape s1(1,2,5);
-    cout << s1 << endl;
-    Shape s2;
-    cout << s2 << endl;
+    Rectangle r1(1.2, 3.4, 10, 20, 30);
+    cout << r1 << endl;
+    cout << r1.surface() << endl;
+    Shape& s1 = r1;
+    cout << s1.surface() << endl; 
+
     return 0;
 }   
 
